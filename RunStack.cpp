0dbaa5dea75024c2414147faf946493be4300fc3
geetikamahajan <geetika00@gmail.com>
2019-11-03 16:54:57 -0500

    basics.bin works now

--- RunStack.cpp
+++ RunStack.cpp
@@ -21,12 +21,19 @@ int RunStack::convertInt(char * mem, int pc){
 
 short RunStack::convertShort(char * mem, int pc){
     int r = mem[pc] & 0xFF;
-    r = (r << 8) | (mem[pc+1] & 0xFF);
+    r = (r) | ((mem[pc+1] & 0xFF) << 8);
     short s = (short)r;
     return s;
 }
 
 float RunStack::convertFloat(char * mem, int pc){
-    return float(((mem[pc] & 0xFF) << 0) | ((mem[pc+1] & 0xFF) << 8) | ((mem[pc+2] & 0xFF) << 16) | ((mem[pc+3] & 0xFF) << 24));
+    float output;
+
+    *((unsigned char*)(&output) + 3) = mem[pc+3];
+    *((unsigned char*)(&output) + 2) = mem[pc+2];
+    *((unsigned char*)(&output) + 1) = mem[pc+1];
+    *((unsigned char*)(&output) + 0) = mem[pc];
+    return output;
+
 }
 
